d9194e4728ec84c7aac4c3f04cf9b3dfcaf9c8f6
AllieStewart <megaxis1217@gmail.com>
2023-12-05 22:10:38 -0600

    added code to /schemas, definitions for typeDefs.js and resolvers.js; still need to finish Mutation in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,47 @@
 // Start of JS file
 // Resolvers for establishing Query and Mutation.
 const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        }
     },
 
     Mutation: {
-
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
+            // return User
+        },
+        removeBook: async (parent, { bookId }) => {
+            // return User
+        },
     },
 };
 
